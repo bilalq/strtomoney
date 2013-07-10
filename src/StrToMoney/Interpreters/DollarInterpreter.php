1714beb543027060a477d6665bafbee9117bfd67
Bilal Quadri <bilalquadri92@gmail.com>
2013-07-10 18:19:36 -0400

    Beginning pattern matching for dollars

--- src/StrToMoney/Interpreters/DollarInterpreter.php
+++ src/StrToMoney/Interpreters/DollarInterpreter.php
@@ -3,4 +3,25 @@
 class DollarInterpreter extends Interpreter {
     public function __construct() {
     }
+
+    public function interpret($string) {
+        $string = trim($string);
+        $lastWord = static::lastWord($string);
+        if (strcasecmp(static::lastWord($string), 'bucks') === 0 ||
+        strcasecmp(static::lastWord($string), 'dollars') === 0) {
+            static::testWordFormat($string);
+        }
+
+    }
+
+    private function testDollarFormat($string) {
+        $matches = array();;
+        $r = '/\$?([0-9]+\.)';
+    }
+
+    private function testWordFormat($string) {
+        $matches = array();;
+    }
+
+
 }
