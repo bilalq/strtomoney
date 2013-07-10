74d61fcaabc4c99ac338bd3854722c832628b7b0
Bilal Quadri <bilalquadri92@gmail.com>
2013-07-10 18:19:16 -0400

    Some utility functions for the interpreter

--- src/StrToMoney/Interpreters/Interpreter.php
+++ src/StrToMoney/Interpreters/Interpreter.php
@@ -1,4 +1,14 @@
 <?php namespace StrToMoney\Interpreters;
 
 abstract class Interpreter {
+    public abstract function interpret($string);
+
+    public function lastWord($string) {
+        return array_pop(explode(' ', $string));
+    }
+
+    public function firstWord($string) {
+        $words = (explode(' ', $string));
+        return empty($words) ? '' : $words[0];
+    }
 }
